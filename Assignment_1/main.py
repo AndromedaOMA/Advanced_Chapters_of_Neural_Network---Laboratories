6600bc15faa41bfb6acec35baa7643465d8b09e4
Olaru Marius-Alexandru <marius.alexandru.olaru@gmail.com>
2025-10-09 17:42:27 +0300

    update

--- Assignment_1/main.py
+++ Assignment_1/main.py
@@ -5,6 +5,7 @@ import os
 import pandas as pd
 import numpy as np
 import time
+
 np.seterr(divide='ignore', invalid='ignore')
 
 
@@ -24,9 +25,6 @@ class ExtendedMNISTDataset(Dataset):
 
     def __getitem__(self, i: int):
         image, label = self.data[i]
-        # Flatten the image and normalize like download_mnist
-        image = np.array(image).flatten()
-        image = (image / 255.0 * 0.99) + 0.01
         return image, label
 
 
@@ -39,26 +37,30 @@ def load_extended_mnist(train=True):
         images.append(image)
         labels.append(label)
 
-    images = np.array(images)
+    images = (np.array(images) / 255.0 * 0.99) + 0.01
     labels = np.array(labels)
 
     # One-hot encode labels
     num_classes = 10
-    one_hot_labels = np.zeros((len(labels), num_classes))
-    one_hot_labels[np.arange(len(labels)), labels] = 1
+    encoded_labels = np.zeros((len(labels), num_classes), dtype=int)
+    encoded_labels[np.arange(len(labels)), labels] = 1
 
-    return images, one_hot_labels
+    return images, encoded_labels
 
 
 train_data, train_labels = load_extended_mnist(train=True)
 test_data, test_labels = load_extended_mnist(train=False)
 
+# verified if one-hot-encoded (True)
+# print(train_labels[0])
+# print(test_labels[0])
+#
+# print('train_data', train_data.shape)
+# print('test_data', test_data.shape)
+
 
 def batches_generator(train_data, train_labels, batch_size):
-    """YIELD (continuous "return") the current batches of 100 elements each"""
-    train_data = np.array(train_data)
-    train_labels = np.array(train_labels)
-
+    """YIELD (continuous "return") the current batches of batch_size elements each"""
     indices = np.arange(len(train_data))
     np.random.shuffle(indices)
 
@@ -96,7 +98,7 @@ def softmax(x, backpropagation=False):
 
 # The MLP architecture should consist of 784 input neurons, 100 hidden neurons, and 10 output neurons.
 class MLP:
-    def __init__(self, in_layer=784, h_layer=100, out_layer=10, lr=0.1, epochs=400, batches=250, dropout_rate=0.1):
+    def __init__(self, in_layer=784, h_layer=100, out_layer=10, lr=0.01, epochs=400, batches=150, dropout_rate=0):
         self.in_layer = in_layer
         self.h_layer = h_layer
         self.out_layer = out_layer
@@ -122,10 +124,16 @@ class MLP:
     def forward_prop(self, train_data, train=True):
         params = self.params_he
 
-        params['A0'] = train_data
-        params['Z1'] = np.add(np.dot(params['W1'], params['A0'].T), params['B1'])
-        # params['Z1'] = np.dot(params['W1'], params['A0'].T) + params['B1']
+        # print('forward_pass')
+        params['A0'] = train_data.T  # train_data.size = (batch_size, 784)
+        # print('A0:', params['A0'].shape)  # (784, batch_size)
+        params['Z1'] = np.add(np.dot(params['W1'], params['A0']), params['B1'])
+        # print('Z1:', params['Z1'].shape)  # (100, batch_size)
+        # print('W1:', params['W1'].shape)  # (100, 784)
+        # print('A0:', params['A0'].shape)  # (784, batch_size)
+        # print('B1:', params['B1'].shape)  # (100, 1)
         params['A1'] = relu(params['Z1'])
+        # print('A1', params['A1'].shape)  # (100, batch_size)
 
         if train:  ## Dropout
             # Step 1: initialize matrix dropout_mask = np.random.rand(..., ...)
@@ -137,41 +145,67 @@ class MLP:
             # Step 4: scale the value of neurons that haven't been shut down
             params['A1'] /= (1 - self.dropout_rate)
             params['dropout_mask'] = dropout_mask
+            # print('A1', params['A1'].shape)  # (100, batch_size)
+            # print('dropout_mask', params['dropout_mask'].shape)  # (100, batch_size)
 
         params['Z2'] = np.add(np.dot(params['W2'], params['A1']), params['B2'])
-        # params['Z2'] = np.dot(params['W2'], params['A1']) + params['B2']
+        # print('Z2:', params['Z2'].shape)  # (10, batch_size)
+        # print('W2:', params['W2'].shape)  # (10, 100)
+        # print('A1:', params['A1'].shape)  # (100, batch_size)
+        # print('B2:', params['B2'].shape)  # (10, 1)
         params['A2'] = softmax(params['Z2'])
+        # print('A2:', params['A2'].shape)  # (10, batch_size)
 
         return params['A2']
 
     def backward_prop(self, prediction, train_labels):
         params = self.params_he
+        batch_size = train_labels.shape[0]
 
-        # err = (prediction - train_labels.T) * softmax(params['Z2'], backpropagation=True)
-        err = (prediction - train_labels.T)
-        params['W2'] -= self.lr * np.dot(err, params['A1'].T) / len(train_labels)
+        err = prediction - train_labels.T
+        # print('prediction:', prediction.shape)  # (10, batch_size)
+        # print('train_labels.T:', train_labels.T.shape)  # (10, batch_size)
+        # print('err: ', err.shape)  # (10, batch_size)
+        params['W2'] -= self.lr * np.dot(err, params['A1'].T) / batch_size
         params['B2'] -= self.lr * np.mean(err, axis=1, keepdims=True)
+        # print('W2: ', params['W2'].shape)  # (10, 100)
+        # print('A1.T: ', params['A1'].T.shape)  # (batch_size, 100)
+        # print('B2: ', params['B2'].shape)  # (10, 1)
 
         err = np.dot(params['W2'].T, err) * relu(params['Z1'], backpropagation=True)
-        # if 'dropout_mask' in params:
-        #     err = np.dot(err, params['dropout_mask'].T) / (1 - self.dropout_rate)
-        params['W1'] -= self.lr * np.dot(err, params['A0']) / len(train_labels)
+        if 'dropout_mask' in params:
+            err = err * params['dropout_mask']
+        # print('err: ', err.shape)  # (100, batch_size)
+        params['W1'] -= self.lr * np.dot(err, params['A0'].T) / batch_size
         params['B1'] -= self.lr * np.mean(err, axis=1, keepdims=True)
+        # print('W1: ', params['W1'].shape)  # (100, 784)
+        # print('A0.T: ', params['A0'].T.shape)  # (batch_size, 784)
+        # print('B1: ', params['B1'].shape)  # (batch_size, 1)
 
-    def compute_acc(self, data, labels):
+    def compute_acc_and_loss(self, data, labels):
         predictions = self.forward_prop(np.array(data), train=False)
         prediction_labels = np.argmax(predictions, axis=0)
         true_labels = np.argmax(labels, axis=1)
-        return np.mean(prediction_labels == true_labels)
+        loss = -np.sum(labels * np.log(predictions.T + 1e-9)) / data.shape[0]
+        return np.mean(prediction_labels == true_labels), loss
 
     def train(self, train_data, train_labels, test_data, test_labels):
         start_time = time.time()
         for epoch in range(self.epochs):
-            for data_batch, label_batch in batches_generator(train_data, train_labels, self.batches):
+            for batch_idx, (data_batch, label_batch) in enumerate(
+                    batches_generator(train_data, train_labels, self.batches)):
                 prediction = self.forward_prop(data_batch)
+
                 self.backward_prop(prediction, label_batch)
-            test_accuracy = self.compute_acc(test_data, test_labels)
-            print(f'Epoch: {epoch + 1}, Time Spent: {time.time() - start_time}s, Test Accuracy: {test_accuracy * 100}%')
+
+            test_accuracy, loss = self.compute_acc_and_loss(test_data, test_labels)
+            print(
+                f'Epoch: {epoch + 1}, Time Spent: {(time.time() - start_time):.2f}s,'
+                f' Test Accuracy: {(test_accuracy * 100):.2f}%, Loss: {loss:.2f}')
+
+    def predict(self, X):
+        output = self.forward_prop(X)
+        return np.argmax(output, axis=1)
 
 
 if __name__ == '__main__':
@@ -193,4 +227,3 @@ if __name__ == '__main__':
     #
     # df = pd.DataFrame(predictions_csv)
     # df.to_csv("submission.csv", index=False)
-
