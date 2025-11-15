9dee87f137af560e86a5d9e20fc0eedb3b6d5fed
Olaru Marius-Alexandru <marius.alexandru.olaru@gmail.com>
2025-11-15 21:45:17 +0200

    update

---
+++ Assignment_3/src/data_pipeline/preprocessing/preprocessing.py
@@ -0,0 +1,34 @@
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+
+def preprocessing(config):
+    train_transformer = transforms.Compose([
+        transforms.RandomAffine(degrees=2, translate=[0.1, 0.1]),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.1307,), std=(0.3081,))
+    ])
+    test_transformer = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.1307,), std=(0.3081,))
+    ])
+
+    if config['dataset']['name'] == 'MNIST':
+        train_dataset = datasets.MNIST(root=f'{config['dataset']['data_dir']}/train', train=True, transform=train_transformer, download=True)
+        test_dataset = datasets.MNIST(root=f'{config['dataset']['data_dir']}/test', train=False, transform=test_transformer, download=True)
+        train_loader = DataLoader(train_dataset, batch_size=config['dataset']['batch_size'], shuffle=True, pin_memory=True)
+        test_loader = DataLoader(test_dataset, batch_size=config['dataset']['batch_size'], shuffle=False, pin_memory=True)
+        return train_loader, test_loader
+    elif config['dataset']['name'] == 'CIFAR10':
+        train_dataset = datasets.CIFAR10(root=f'{config['dataset']['data_dir']}/train', train=True, transform=train_transformer, download=True)
+        test_dataset = datasets.CIFAR10(root=f'{config['dataset']['data_dir']}/test', train=False, transform=test_transformer, download=True)
+        train_loader = DataLoader(train_dataset, batch_size=config['dataset']['batch_size'], shuffle=True, pin_memory=True)
+        test_loader = DataLoader(test_dataset, batch_size=config['dataset']['batch_size'], shuffle=False, pin_memory=True)
+        return train_loader, test_loader
+    elif config['dataset']['name'] == 'CIFAR100':
+        train_dataset = datasets.CIFAR100(root=f'{config['dataset']['data_dir']}/train', train=True, transform=train_transformer, download=True)
+        test_dataset = datasets.CIFAR100(root=f'{config['dataset']['data_dir']}/test', train=False, transform=test_transformer, download=True)
+        train_loader = DataLoader(train_dataset, batch_size=config['dataset']['batch_size'], shuffle=True, pin_memory=True)
+        test_loader = DataLoader(test_dataset, batch_size=config['dataset']['batch_size'], shuffle=False, pin_memory=True)
+        return train_loader, test_loader
---
+++ Assignment_3/src/training/scripts/train/train_model.py
@@ -0,0 +1,11 @@
+from Assignment_3.src.data_pipeline.preprocessing.preprocessing import preprocessing
+from Assignment_3.src.training.utils.load_config import load_config
+
+if __name__ == '__main__':
+    config = load_config("../experiments/experiment1/config.yml")
+
+    print(f'Running the {config['experiment']['name']} experiment')
+    device = config['experiment']['device']
+
+    print(f'Device - {config['experiment']['device']}')
+    train_loader, test_loader = preprocessing(config)
